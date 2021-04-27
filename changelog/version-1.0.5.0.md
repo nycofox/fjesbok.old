4b718ac34a1f6d245f4ecbffd05d7054ed580462
Nyco Fox <{ID}+{username}@users.noreply.github.com>
2021-04-27 18:39:28 +0000

    Added Log Reader

---
+++ changelog/version-1.0.5.0.md
@@ -0,0 +1,11 @@
+## 1.0.5.0
+
+2021-05-02
+
+Some bugfixes
+
+### Minor Updates
+
+#### Fixed
+
+- Fixed a couple of bugs with Webcomics
--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -14,6 +14,9 @@
     >
         <a href="{{ route('admin.users.index') }}" class="pl-2">Users</a>
     </div>
+    <div class="flex cursor-pointer border px-4 py-2 text-lg text-grey-darkest">
+        <a href="{{ url(config('laravel-log-reader.view_route_path')) }}" class="pl-2">Log Reader</a>
+    </div>
 
     <div class="uppercase tracking-wide text-c2 mb-4 mt-8">Modules</div>
     <div class="flex cursor-pointer border px-4 py-2 text-lg text-grey-darkest"
