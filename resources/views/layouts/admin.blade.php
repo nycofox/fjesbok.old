1e5d734074b81ac1cfabf36a2fbb54a354dfff98
Nyco Fox <{ID}+{username}@users.noreply.github.com>
2021-05-17 08:52:03 +0000

    Push before prod

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
-    <base href="../"/>
+    <base href="{{ url('/') }}/"/>
 
     <title>Admin - {{ config('app.name') }}</title>
 
