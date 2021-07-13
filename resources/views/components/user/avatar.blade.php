d2a6fbd2830211fb11658b5721ff5e85f4764da2
Anders <anders@pecon.no>
2021-07-13 14:46:28 +0000

    Avatar displays offline status

--- resources/views/components/user/avatar.blade.php
+++ resources/views/components/user/avatar.blade.php
@@ -3,6 +3,8 @@
         <img class="rounded-full" alt="{{ $user->name }}" src="{{ $user->avatar }}">
         @if(Cache::has('user-online-' . $user->id))
             <div class="bg-green-500 border-solid border-white border-2 rounded-full w-4 h-4 absolute bottom-0 right-0 p-1"></div>
+        @else
+            <div class="bg-gray-400 border-solid border-white border-2 rounded-full w-4 h-4 absolute bottom-0 right-0 p-1"></div>
         @endif
     </a>
 </div>
